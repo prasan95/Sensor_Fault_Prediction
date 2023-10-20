2032f58aeee3e247074c8530468637256ef78b85
Pravin1497 <pravinpawar7128a@gmail.com>
2023-10-20 16:30:19 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prashant",
+    author_email="prasan.sp95@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+AWS_S3_BUCKET_NAME="Sesor-Fault"
+MONGO_DATABASE_NAME='PRASHANT'
+MONGO_COLLECTION_NAME="SensorFault"
+
+TARGET_COLUMN= "quality"
+MONGO_DB_URL="mongodb+srv://Prasansp95:Prasansp95@cluster0.edz9m9q.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME='model'
+MODEL_FILE_EXTENSION= '.pkl'
+
+artifact_folder="artifacts"
